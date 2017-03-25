e62835c58d3807c152e3bfb7298fe38aa260074d
JSlovak <jasmine.slovak@gmail.com>
2017-03-25 10:31:23 -1000

    New global var pointerColor adds selected color to canvas.

--- js/canvas.js
+++ js/canvas.js
@@ -1,5 +1,30 @@
 var canvas = document.createElement("div");
+  canvas.id = "canvas";
+  pixelPainter.appendChild(canvas);
 
-canvas.id = "swatchContainer";
+  for(var i=0; i<10; i++){
+    var row = document.createElement("div");
+    // row.className = "row"+i.toString();//"row0","row1"...
+    canvas.appendChild(row);
 
-pixelPainter.appendChild(swatchContainer);
+      for(var j=0; j<10; j++){
+        var pixel = document.createElement("div");
+        pixel.id = "row" + i + "pixel" + j;
+        pixel.style.width = "25px";
+        pixel.style.height = "25px";
+        pixel.style.border = "1px solid black";
+        pixel.style.display = "table-cell";
+        pixel.style.background = "beige";
+        pixel.addEventListener("click", function(event){
+          console.log(event.target.id);
+          console.log(pointerColor);
+          event.target.style.background = pointerColor;
+        });
+        row.appendChild(pixel);
+      }
+}
+
+function target(){
+  return this.id;
+  console.log(this.id);
+}
--- js/swatches.js
+++ js/swatches.js
@@ -5,7 +5,7 @@ swatchContainer.id = "swatchContainer";
 
 pixelPainter.appendChild(swatchContainer);
 
- class Swatches{
+ class Swatch{
   constructor(id, clss, colorValue){
     this._id = id;
     //this is class
@@ -15,11 +15,10 @@ pixelPainter.appendChild(swatchContainer);
     this.width = 25;
    }
 }
-
 // var testSwatch = new Swatches(1,"#4682b4");
 // console.log(testSwatch);
+var pointerColor = "blank";
 
-//if we change "2"
 for(var i=0; i<2; i++){
   var row = document.createElement("div");
   // row.className = "row"+i.toString();//"row0","row1"...
@@ -31,10 +30,17 @@ for(var i=0; i<2; i++){
     swatch.style.height = "25px";
     swatch.style.border = "1px solid black";
     swatch.style.display = "table-cell";
-
+    swatch.addEventListener("click", function(event){
+      // console.log(event.target.style.background);
+      console.log(pointerColor);
+      pointerColor = event.target.style.background;
+      console.log(pointerColor);
+      // return pointerColor;
+    });
     row.appendChild(swatch);
   }
 }
+
 var pixelRed = document.getElementById("row0swatch0");
   pixelRed.style.background = "red";
 
@@ -46,5 +52,7 @@ pixelYellow.style.background = "yellow";
 
 var pixelGreen = document.getElementById('row1swatch1');
 pixelGreen.style.background = "green";
+
+
 
 
