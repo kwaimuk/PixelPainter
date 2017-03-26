30f76fb06417e6000695e46a992212092b8dcf5d
JSlovak <jasmine.slovak@gmail.com>
2017-03-25 16:39:20 -1000

    Recommitting prev changes, trouble shooting git, and git pull

--- js/app.js
+++
@@ -1,9 +0,0 @@
-
-
-// Click & Drag to paint pixels
-
-
-var isMouseDown = false;
-document.onmousedown = function() { isMouseDown = true;};
-document.onmouseup   = function() { isMouseDown = false;};
-
