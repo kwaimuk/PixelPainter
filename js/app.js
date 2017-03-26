531fb23be7cbdc83cab54a5299fdde253ba5ad05
Ernest Tsang <kwaimuk@Ernests-MacBook-Air.local>
2017-03-25 16:40:52 -1000

    test

--- js/app.js
+++ js/app.js
@@ -3,7 +3,3 @@
 // Click & Drag to paint pixels
 
 
-var isMouseDown = false;
-document.onmousedown = function() { isMouseDown = true;};
-document.onmouseup   = function() { isMouseDown = false;};
-
