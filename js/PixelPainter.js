4ecd30ce8356063cb4776e687c806a551a5a2840
Ernest Tsang <kwaimuk@Ernests-MacBook-Air.local>
2017-03-27 14:38:06 -1000

    modules

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -8,3 +8,12 @@ console.log(features);
 console.log(features.buttonContainer());
 console.log(features.clickDrag());
 console.log(features.eraseButton());
+console.log(features.clearButton());
+console.log(features.liteBriteMode());
+
+
+console.log(swatches);
+console.log(swatches.swatchContainer());
+console.log(swatches.swatches());
+
+
--- js/swatches.js
+++ js/swatches.js
@@ -1,44 +1,50 @@
 
 var pointerColor = "blank";
 
-var swatchContainer = (function(){
-  var container = document.createElement("div");
-  container.id = "container";
-  pixelPainter.appendChild(container);
+var swatchModule = (function(){
+
+  function swatchContainer(){
+    var container = document.createElement("div");
+    container.id = "container";
+    pixelPainter.appendChild(container);
+
+  }
+
+  function swatches(){
+    for(var i=0; i<10; i++){
+      var row = document.createElement("div");
+     container.appendChild(row);
+        for(var j=0; j<5; j++){
+          var swatch = document.createElement("div");
+          swatch.id = "row" + i + "swatch" + j;
+          swatch.className = "swatch";
+          swatch.style.width = "25px";
+          swatch.style.height = "25px";
+          swatch.className = "swatch";
+          swatch.style.border = "1px solid white";
+          swatch.style.display = "table-cell";
+          swatch.addEventListener("click", function (event){
+          pointerColor = event.target.style.background;
+          });
+        row.appendChild(swatch);
+        }
+    }
+    // Assign color values
+    var x = document.querySelectorAll(".swatch");
+    var i;
+    var colorArr = ["rgb(153,0,0)", "rgb(204,0,0)", "rgb(255,0,0)", "rgb(255,51,51)", "rgb(255,102,102)","rgb(153,76,0)"];
+
+    for(i = 0; i< x.length; i++) {
+       x[i].style.background = colorArr[i];
+    }
+  }
+
+    return {
+    swatchContainer: swatchContainer,
+    swatches: swatches
+  };
 
 })();
-
-var swatches = (function() {
-
-  for(var i=0; i<10; i++){
-    var row = document.createElement("div");
-   container.appendChild(row);
-      for(var j=0; j<5; j++){
-        var swatch = document.createElement("div");
-        swatch.id = "row" + i + "swatch" + j;
-        swatch.className = "swatch";
-        swatch.style.width = "25px";
-        swatch.style.height = "25px";
-        swatch.className = "swatch";
-        swatch.style.border = "1px solid white";
-        swatch.style.display = "table-cell";
-        swatch.addEventListener("click", function (event){
-        pointerColor = event.target.style.background;
-        });
-      row.appendChild(swatch);
-      }
-  }
-  // Assign color values
-  var x = document.querySelectorAll(".swatch");
-  var i;
-  var colorArr = ["rgb(153,0,0)", "rgb(204,0,0)", "rgb(255,0,0)", "rgb(255,51,51)", "rgb(255,102,102)","rgb(153,76,0)"];
-
-  for(i = 0; i< x.length; i++) {
-     x[i].style.background = colorArr[i];
-  }
-})();
-
-
 
 
 
