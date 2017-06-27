d90a37ab016cb79332d7d6bd67d097bdb07fb5e2
kwaimuk <teaelixer@gmail.com>
2017-06-27 09:03:55 -1000

    another one

--- js/swatches.js
+++ js/swatches.js
@@ -33,7 +33,8 @@ console.log("title"+pixelPainter);
     // Assign color values
     var x = document.querySelectorAll(".swatch");
     var i;
-    var colorArr = ["white", "silver","gray","black","red","marron","yellow","olive","lime","green","aqua","teal","blue","navy","fuchsia","purple"];
+    var colorArr = ["black" ,"navy" ,"darkblue" ,"mediumblue" ,"blue" ,"darkgreen" ,"green" ,"teal" ,"darkcan" ,"forestgreen" ,"seagreen" ,"darkslategray" ,"darkviolet" ,"palegreen" ,"darkorchid" ,"yellowgreen" ,"sienna" ,"brown" ,"darkgray" ,"lightsteelblue" ,"powderblue" ,"firebrick" ,"darkgoldenrod" ,"mediumorchid" ,"rosybrown" ,"darkkhaki" ,"silver" ,"mediumvioletred" ,"indianred " ,"peru" ,"chocolate" ,"tan" ,"lightgray" ,"lightgrey" ,"thistle" ,"orchid" ,"goldenrod" ,"palevioletred" ,"crimson" ,"darksalmon" ,"violet" ,"palegoldenrod" ,"lightcoral" ,"khaki" ,"aliceblue" ,"honeydew" ,"azure" ,"sandybrown" ,"wheat" ,"beige" ,"whitesmoke" ,"mintcream" ,"ghostwhite" ,"salmon" ,"antiquewhite" ,"linen" ,"lightgoldenrodyellow" ,"oldlace" ,"red" ,"fuchsia" ,"magenta" ,"deeppink" ,"orangered" ,"tomato" ,"hotpink" ,"coral" ,"darkorange" ,"lightsalmon" ,"orange" ,"lightpink" ,"pink" ,"gold" ,"peachpuff" ,"navajowhite" ,"moccasin" ,"bisque" ,"mistyrose" ,"blanchedalmond" ,"papayawhip" ,"lavenderblush" ,"seashell" ,"cornsilk" ,"lemonchiffon" ,"floralwhite" ,"snow" ,"yellow" ,"lightyellow" ,"ivory" ,"white"
+];
 
     for(i = 0; i< x.length; i++) {
        x[i].style.background = colorArr[i];
